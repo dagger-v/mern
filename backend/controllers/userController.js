4f33c55375823847d759f2f9fd15e8bc3a38ac8c
dagger-v <goingarcher@gmail.com>
2023-10-11 02:56:50 -0700

    updated auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,61 @@
 import asyncHandler from "express-async-handler";
+import generateToken from "../utils/generateToken.js";
+import User from "../models/userModel.js";
 
 // POST /api/users/auth
 const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Auth User" });
 });
 
-export { authUser };
+// POST /api/users
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Already Exists!");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User Data");
+  }
+});
+
+// POST /api/users/logout
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Logout User" });
+});
+
+// GET /api/users/profile
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "User Profile" });
+});
+
+// PUT /api/users/profile
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Update User Profile" });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
